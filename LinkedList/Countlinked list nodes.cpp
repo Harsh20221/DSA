563e2cd584354e157ee8a128d71d9726badaff5d
Harsh Kumar <harsh20221@gmail.com>
2024-01-20 13:11:06 +0530

    Count displayrecursive and sum

--- LinkedList/Countlinked list nodes.cpp
+++ LinkedList/Countlinked list nodes.cpp
@@ -22,22 +22,24 @@ for (i=1 ; i<n;i++){
    last=r;
 }
 }
-void displayrecursive(struct node *p){
+void displayrecursive(struct node *p){  //! Make sure to pass struct node p to display parameter , this will be applicable for both recursive and iterative approach 
     if(p!=NULL){  //! Make sure to give if condition to display function 
     cout<<p->data<<endl;
 displayrecursive(p->next);}  //! Do not write p=p->next in display function 
                             ///! write p->next only 
 }
 
-void count (struct node *p ){
-int c=0;
+void count (struct node *p ){  //! Make sure to pass struct p as parameter to count function    
+int c=0;    //* Fun fact-- In order to write count or sum or similar functions that iterates through the entire linked list , you just need to remember the display funnction and modify it accordingly to count or sum 
 while(p!=NULL){
     c++;
     p=p->next;
 }
 cout<<c<<endl;   //! Do not write cout p here you need to print the count not the struct p 
 }
+void sum(){
 
+}
 
 int main (){
  int  A[]={2,3,4,5,6,7,8};
