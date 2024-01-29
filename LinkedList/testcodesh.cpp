4fe5d9b23c9b0f8b981078cb979dc9578131b8d6
Harsh Kumar <harsh20221@gmail.com>
2024-01-29 22:31:43 +0530

    test

---
+++ LinkedList/testcodesh.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){ 
+  //tis is for next line Code
+  /* I'm Tring to do multiple line Code I think that is right code */
+printf("hlo shivam\n");
+printf("how are you shivam\n");
+printf("are you mad shivam?\n");
+
+//real, char and integer of special code 
+char name = 'shivam';  //! Single quote use hoga charecter assign karne waqt 
+float marks = 97.6;
+int rollno = 30; //! The name of this variable name will not be written like roll no , there should not be any space between the variable name
+printf("my roll no is %d \n",rollno);
+printf("my name is %s \n",name);
+printf("my marks is %f \n", marks ); 
+  return 0;
+}
