50660108228b8d60c4f559155f7e7fa698caf9d7
Harsh Kumar <harsh20221@gmail.com>
2024-04-28 05:37:58 +0530

    Queue using linked list

---
+++ Graph/Linked List Queue for Graphs.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+
+struct node{
+int data;
+ node *next =NULL;
+}*front=NULL,*rear=NULL;
+
+void enqueue(int x) {
+    struct node *t;
+    t=(struct node*)malloc(sizeof(struct node));
+    if(t==NULL){
+        printf("Queue is full\n");
+    }
+    else  t->data=x;
+    t->next=NULL;
+    if(front==NULL){
+        front=rear=t;    
+    }
+    else
+    rear->next=t;
+    rear=t;
+
+    }
+
+int dequque(){
+    int x=-1;
+    node *t;
+    if(front==NULL){
+    printf("Queue is Empty no element to process ");}
+    else {
+   x=front->data;
+   t=front;
+   front=front->next;
+   free(t);}
+   return x;
+}
+
+void display(){
+    struct node *t=front;
+    while (t!=NULL){
+    printf("%d",t->data);
+    t=t->next;
+    }
+}
+
+int main(){
+    enqueue(40);
+    enqueue(50);
+    enqueue(70);
+    enqueue(80);
+    dequque();
+    display();
+
+
+}
