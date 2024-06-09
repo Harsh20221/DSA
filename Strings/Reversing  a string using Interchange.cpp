3c933151afe803ebd3d90b28d2359af433023b2f
Harsh Kumar <harsh20221@gmail.com>
2024-06-09 08:02:20 +0530

    commitments

--- Strings/Reversing  a string using Interchange.cpp
+++ Strings/Reversing  a string using Interchange.cpp
@@ -4,7 +4,7 @@
 #include <cstring>
 using namespace std;
 int main(){
-    char A[]={"DUALIPA"};
+    char A[]={"DUA LIPA"};
     int i=0 ; int j = strlen(A)-1; 
     char temp; //! Very important to assign temp as character and not as integer
     for(i=0 , j=strlen(A)-1 ; i<j ; i++ , j--){ // !! Use for Loop here and not while loop // Mke sure to write the j staring point as strlen A -1 and not strlen A
